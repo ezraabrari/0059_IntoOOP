7a93300ebe1157b751bbd0dd31cdc7e4094ec156
ezraabrari <ezraabrari13@gmail.com>
2024-04-22 17:36:50 +0900

    menampilkan luas

--- intoOOP2.cpp
+++ intoOOP2.cpp
@@ -22,5 +22,6 @@ class bangunDatar{
     void display() {
         cout << "Panjangnya = " << panjang << endl;
         cout << "Lebarnya = " << lebar << endl;
+        cout << "Luasnya = " << luas << endl;
     }
 }
